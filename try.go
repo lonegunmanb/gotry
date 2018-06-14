f9bc22fbdb9eab3638efbd397fcd1884610ce6c4
zjhe <zjhe@beyondh.com>
2018-06-14 12:07:44 +0800

    Add ExecuteWithRetryHook, add retry on invalid return or error

--- try.go
+++ try.go
@@ -1,20 +1,30 @@
 package gotry
 
-type ReturnValue interface {
-	IsSuccess() bool
-}
 
-type Func func() (ReturnValue, error)
+type Func func() (interface{}, bool, error)
+type OnRetryFunc func(interface{}, error)
 type Method func() (error)
 
 type Policy struct{
 	retryAttempt int
-
 }
 
 func (policy *Policy) SetRetry(retryAttempt int) {
 
 }
-func (policy *Policy) Execute(successFunc Func) (ReturnValue, error) {
-	return successFunc()
+func (policy *Policy) Execute(funcBody Func) (interface{}, bool, error) {
+	return policy.ExecuteWithRetryHook(funcBody, nil)
+}
+
+func(policy *Policy) ExecuteWithRetryHook(funcBody Func, onRetry OnRetryFunc) (returnValue interface{}, isReturnValueValid bool, err error) {
+	for i := policy.retryAttempt; i >= 0; i-- {
+		returnValue, isReturnValueValid, err = funcBody()
+		if err == nil && isReturnValueValid {
+			return returnValue, true, nil
+		}
+		if onRetry != nil {
+			onRetry(returnValue, err)
+		}
+	}
+	return
 }
--- try_test.go
+++ try_test.go
@@ -4,35 +4,81 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"errors"
 )
 
-type Integer int
+const ExpectedReturnValue = 1
 
-const ExpectedReturnValue Integer = 1
-
-func (input Integer) IsSuccess() bool{
-	return input == ExpectedReturnValue
-}
+var ExpectedError = errors.New("expectedError")
 
 type TryTestSuite struct {
 	suite.Suite
 	policy *Policy
+	retried bool
 }
 
 func (suite *TryTestSuite) SetupTest() {
 	suite.policy = &Policy{}
 	suite.policy.SetRetry(1)
+	suite.retried = false
 }
 
 func (suite *TryTestSuite) TestSuccessFunc(){
-	var successFunc Func = func() (ReturnValue, error){
-		var returnValue = ExpectedReturnValue
-		return returnValue, nil
+	var successFunc Func = func() (interface{}, bool, error) {
+		var returnValue= ExpectedReturnValue
+		return returnValue, returnValue > 0, nil
 	}
-	var returnValue, err = suite.policy.Execute(successFunc)
+	var returnValue, isReturnValueValid, err = suite.policy.Execute(successFunc)
 	assert.Nil(suite.T(), err)
-	assert.IsType(suite.T(), ExpectedReturnValue, returnValue)
+	assert.True(suite.T(), isReturnValueValid, "success invoke's return value should pass check")
 	assert.Equal(suite.T(), ExpectedReturnValue, returnValue, "should be equal")
+	assert.False(suite.T(), suite.retried)
+}
+
+func (suite *TryTestSuite) TestInvalidReturnValueFunc() {
+	var invalidReturnFunc Func = func() (interface{}, bool, error) {
+		var returnValue= ExpectedReturnValue
+		return returnValue, returnValue < 0, nil
+	}
+	onRetryHook := func(interface{}, error) {
+		suite.retried = true
+	}
+	var returnValue, isReturnValueValid, err = suite.policy.ExecuteWithRetryHook(invalidReturnFunc,
+																				   onRetryHook)
+	assert.Nil(suite.T(), err)
+	assert.False(suite.T(), isReturnValueValid, "invalid return value should cause retry")
+	assert.Equal(suite.T(), ExpectedReturnValue, returnValue,
+		"invalid return value should be returned")
+	assert.True(suite.T(), suite.retried)
+}
+
+func (suite *TryTestSuite) TestErrorFunc() {
+	var errorFunc = func() (interface{}, bool, error) {
+		return ExpectedReturnValue, true, ExpectedError
+	}
+	onRetryHook := func(interface{}, error) {
+		suite.retried = true
+	}
+	var returnValue, isRetrunValueValid, err = suite.policy.ExecuteWithRetryHook(errorFunc, onRetryHook)
+	assert.Equal(suite.T(), ExpectedReturnValue, returnValue)
+	assert.True(suite.T(), isRetrunValueValid)
+	assert.Equal(suite.T(), ExpectedError, err)
+	assert.True(suite.T(), suite.retried)
+}
+
+func (suite *TryTestSuite) MultipleRetry() {
+	const RetryAttempt = 5
+	suite.policy.SetRetry(RetryAttempt)
+	var retryCount = 0
+	var errorFunc = func() (interface{}, bool, error) {
+		return ExpectedReturnValue, true, ExpectedError
+	}
+	onRetryHook := func(interface{}, error) {
+		retryCount++
+	}
+	var _, _, err = suite.policy.ExecuteWithRetryHook(errorFunc, onRetryHook)
+	assert.Equal(suite.T(), ExpectedError, err)
+	assert.Equal(suite.T(), RetryAttempt, retryCount)
 }
 
 func TestTrySuite(t *testing.T) {
