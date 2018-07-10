2ad015d216c5f9853cde1c28b8f411f55449a5d1
lonegunman <lonegunmanb@hotmail.com>
2018-07-10 10:59:09 +0800

    Add OnCancel event support

--- cancellation.go
+++ cancellation.go
@@ -8,15 +8,35 @@ type Cancellation interface {
 	IsCancellationRequested() bool
 	Cancel() bool
 }
+type OnCancel func()
 
 type cancellation struct {
 	isCancellationRequestedFlag int32
+	onCancel OnCancel
+	
 }
 func (cancellation *cancellation) IsCancellationRequested() bool {
 	return atomic.LoadInt32(&cancellation.isCancellationRequestedFlag) == cancellationRequestedFlag
 }
 func (cancellation *cancellation) Cancel() bool {
+	if successfulCanceled(cancellation) {
+		notifyOnCancel(cancellation)
+		return true
+	}
+	return false
+}
+
+func notifyOnCancel(cancellation *cancellation) {
+	if cancellation.onCancel != nil{
+		cancellation.onCancel()
+	}
+}
+
+func successfulCanceled(cancellation *cancellation) bool {
 	return atomic.CompareAndSwapInt32(&cancellation.isCancellationRequestedFlag,
-													cancellationNotRequestedFlag,
-													cancellationRequestedFlag)
+		cancellationNotRequestedFlag,
+		cancellationRequestedFlag)
+}
+func (cancellation *cancellation) OnCancel(onCancel OnCancel) {
+	cancellation.onCancel = onCancel
 }
--- cancellation_test.go
+++ cancellation_test.go
@@ -3,7 +3,16 @@ package gotry
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type mockCancel struct {
+	mock.Mock
+}
+
+func (mockCancel *mockCancel) OnCancel(){
+	mockCancel.Called()
+}
 
 func TestCancel(t *testing.T){
 	cancellation := &cancellation{}
@@ -19,4 +28,13 @@ func TestTwiceCancel(t *testing.T){
 	cancelRequested := cancellation.Cancel()
 	assert.False(t, cancelRequested)
 	assert.True(t, cancellation.IsCancellationRequested())
+}
+
+func TestOnCancel(t *testing.T){
+	cancellation := &cancellation{}
+	cancelHook := &mockCancel{}
+	cancelHook.On("OnCancel").Return()
+	cancellation.OnCancel(func(){cancelHook.OnCancel()})
+	cancellation.Cancel()
+	cancelHook.AssertCalled(t, "OnCancel")
 }
